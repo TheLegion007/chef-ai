32e5a01ceeab4485ce48e7d9d862a086f2656d44
TheLegion007 <manisksing@gmail.com>
2024-12-05 17:36:41 +0530

    list component added

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,33 @@
 export default function Main() {
+    
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map((ingredient) => {
+        return <li key={ingredient}>{ingredient}</li>
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")  // name from form > input //
+        ingredients.push(newIngredient);
+        console.log("value : " + ingredients);
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button> Add ingredient</button>
-                
+                <button> Add ingredient</button> 
             </form>
+            <ul>
+                {}
+            </ul>
         </main>
     )
 }
