acabdd6bf0ecadfeab2b6703f07456d873a58b6e
TheLegion007 <manisksing@gmail.com>
2024-12-10 00:16:03 +0530

    Conditional Rendering for recipe component

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 
 export default function Main() {
-    
-    //const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredients_1 = ["Chicken", "Oregano", "Tomatoes","Olive Oil", "Spinach"]
     const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
 
     const ingredientsListItems = ingredients.map((ingredient) => {
@@ -22,23 +22,37 @@ export default function Main() {
         console.log("formData : ", formData)
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-        console.log("newingredient: " ,newIngredient);
+        console.log("newingredient: ", newIngredient);
     }
 
     return (
         <main>
             <form onSubmit={addIngredient} className="add-ingredient-form">
-                <input 
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                     name="ingredient"
                 />
-                <button> Add ingredient</button> 
+                <button> Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+
+            {ingredients.length > 0 &&
+                <section>
+                    <h2>Ingredients on Hand :</h2>
+                    <ul className="ingredients-list" aria-live="polite">
+                        {ingredientsListItems}
+                    </ul>
+                    {ingredients.length > 3 && <div className="get-recipe-container">
+                        <div>
+                            <h3>Ready for a recipe?</h3>
+                            <p>Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button>Get a recipe</button>
+                    </div>}
+                </section>
+            }
+
         </main>
     )
 }
